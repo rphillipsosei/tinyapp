47e14d3be659ccf40e02e711e4728670f4b7efb2
Rhoda Phillips Osei <rhodaphillipsosei@gmail.com>
2022-01-27 20:03:55 +0000

    review with mentor on all edits, start of login route refactoring

--- express_server.js
+++ express_server.js
@@ -14,6 +14,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 const users = { 
   "userRandomID": {
     id: "userRandomID", 
@@ -30,7 +31,7 @@ const users = {
 const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 function generateRandomString(length) {
-    let result = ' ';
+    let result = '';
     const charactersLength = characters.length;
     for ( let i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -39,7 +40,15 @@ function generateRandomString(length) {
     return result;
 }
 
-
+//function to check if a user exists in the database(reg errors)
+function checkUser(email) {
+  for (let element in users) {
+    if (users[element].email === email) {
+      return true;
+    }
+  }
+  return false;
+}
 
 app.set("view engine", "ejs")
 
@@ -56,13 +65,12 @@ app.get("/hello", (req, res) => {
   });
 
 app.get("/urls", (req, res) => {
-  console.log(req.cookies.username)
-  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
+  const templateVars = {urls: urlDatabase, user: users[req.cookies["user_id"]]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = {username: req.cookies["username"]};
+  const templateVars = {user: users[req.cookies["user_id"]]};
   res.render("urls_new", templateVars);
 });
 
@@ -87,17 +95,33 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 })
 
 app.post("/login", (req, res) => {
-res.cookie("username", req.body.username)
+res.cookie("user", users[req.body.user_id])
 res.redirect("/urls")
+
+const idKey = user_id 
+const email = req.body.email;
+const password = req.body.password;
+
+if (!email || !password || !users[email].include(email) || !users[password].include(password)) {
+  return res.status(400).send("Please enter valid email and/or password.");
+}
+const userExist = checkUser(email);
+
+if (userExist && password === users[password]) {
+res.cookie("user_id", idKey);
+res.redirect("/urls")
+};
+
+
 })
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username", req.body.username)
+  res.clearCookie("user_id")
   res.redirect("/urls")
 })
 
 app.get('/register', (req, res) => {
-  const templateVars = {username: null}
+  const templateVars = {user: null}
   res.render("register", templateVars)
 })
 
@@ -106,31 +130,36 @@ app.post('/register', (req, res) => {
   const idKey = generateRandomString(10)
   const email = req.body.email;
   const password = req.body.password;
- //object to be filled out by client inputs then pushed to users(new users)
-  const newUser = {
-    id: idKey,
-    email: email,
-    password: password,
-  };
-  function checkUser(email) {
-    for (element in users) {
-      if (users[element].email === email) {
-        return true;
-      }
-    }
-    return false;
-  }
+
   
-  if (!email || password === " " || !password) {
-    return res.status(403).send("Please enter valid credentials.");
+//conditionals for reg errors (reg errors)
+  if (!email || !password) {
+    return res.status(403).send("Please enter email and/or password.");
   }
   const userExist = checkUser(email);
 
   if (userExist) {
     return res.status(403).send("Sorry, user already exists!");
   };
+  //object to be filled out by client inputs then pushed to users(new users)
+  const newUser = {
+    id: idKey,
+    email: email,
+    password: password,
+  };
+
+  //add newUser to users obj
+users[idKey] = newUser
+
 //cookie recording generated id(new usesrs)
   res.cookie("user_id", idKey);
+
   //redirect to urls (new users)
   res.redirect("/urls")
+});
+
+//login get req (new login page)
+app.get('/login', (req, res) => {
+  const templateVars = {user: null} //not correct, how to access required i
+  res.render("login", templateVars)
 })
