f39a153efb9e7c43ed3599fd9f51bf63f799f5f6
Rhoda Phillips Osei <rhodaphillipsosei@gmail.com>
2022-01-27 02:14:02 +0000

    Add register GET endpoint, forms and buttons on register page

--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@ const PORT = 8080;
 const cookieParser = require('cookie-parser');
 
 const bodyParser = require("body-parser");
+const { response } = require("express");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser())
 
@@ -12,6 +13,19 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -79,5 +93,11 @@ res.redirect("/urls")
 
 app.post("/logout", (req, res) => {
   res.clearCookie("username", req.body.username)
-  res.redirect("urls")
+  res.redirect("/urls")
 })
+
+app.get('/register', (req, res) => {
+  const templateVars = {username: null}
+  res.render("register", templateVars)
+})
+
